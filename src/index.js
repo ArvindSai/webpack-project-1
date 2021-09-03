10817c6b8e6f2283e5bbb48348e059f3858d6b47
Arvind V <arvind5413@gmail.com>
2021-09-02 20:32:43 -0400

    Initial Webpack Commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const myObject = {
+    "openers": {
+        "striker": "Rohit",
+        "non-striker": "Rahul",
+    },
+    "#3": "Cheteswar Pujara",
+    "#4": "Virat Kohli",
+};
+
+console.log(myObject);
